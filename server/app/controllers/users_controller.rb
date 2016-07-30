0232a0b55084043b72ab3ab4930627b514e495a3
Or Perlman <hikariJ@gmail.com>
2016-07-31 00:50:26 +0300

    Adding check for existing email for registration process

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -2,32 +2,51 @@ class UsersController < Devise::RegistrationsController
     respond_to :json
 
     def create
-        @user = User.find_by_email(user_params[:email].downcase)
-        if @user.nil?
-            @user = User.new(user_params)
-            @user.save
-        else
+        if email_exists?
             render status: 409,
-                json: { success: false, error: 'User already exists' }
+                json: { response: "User already registered with email #{user_params[:email]}.\r\nPlease register with a different email." }
             return
         end
+
+        @user = User.new(user_params)
+        @user.save
 
         if doorkeeper_oauth_client and @user
             @access_token ||= Doorkeeper::AccessToken.create!(application_id: DOORKEEPER_APP_ID, resource_owner_id: @user.id, scopes: "public write preferences", use_refresh_token: true, expires_in: Doorkeeper.configuration.access_token_expires_in)
             render status: 201,
-                json: Doorkeeper::OAuth::TokenResponse.new(@access_token).body,
-                each_serializer: UserSerializer
+                json: { response: Doorkeeper::OAuth::TokenResponse.new(@access_token).body.merge(user: @user) }
             return
         else 
             warden.custom_failure!
-            render json: { success: false, error: @user.errors }, status: 422
+            render json: { response: @user.errors, success: false }, status: 422
         end
+    end
+
+    def email_exists
+        if email_exists?
+            render status: 409,
+                json: { response: "User already registered with email #{user_params[:email]}.\r\nPlease register with a different email." }
+            return
+        end
+
+        render status: 200,
+            json: { response: "Email #{user_params[:email]} is not registered in the system." }
+        return
     end
 
 private
 
+    def email_exists?
+        @user = User.find_by_email(params[:email].downcase)
+        if @user.present?
+            return true
+        end
+
+        return false
+    end
+
     def user_params
-        params.permit(:client_id, :client_secret, :email, :first_name, :last_name, 
+        params.permit(:id, :email, :first_name, :last_name, 
                       :password, :password_confirmation)
     end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   use_doorkeeper
   devise_for :users, controllers: { registrations: 'users' }
 
+  devise_scope :user do
+    post 'users/email_exists', :to => 'users#email_exists'
+  end
+
   namespace :api, defaults: {format: "json"} do
     scope module: :v1 do
       
